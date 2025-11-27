9f99a9156fd70a2adc1182f5ae20ca9c37ae89f7
Caleb Hill <calebthill@gmail.com>
2025-11-27 00:38:38 +0000

    act vs predic test

--- calculate_stall_rate.py
+++ calculate_stall_rate.py
@@ -53,13 +53,13 @@ def load_data_with_retry(func, name, max_retries=5, delay=5):
 
 def get_current_nfl_week():
     try:
-        schedule = load_data_with_retry(lambda: nfl.load_schedules(seasons=[CURRENT_SEASON]), "Schedule Check")
-        if hasattr(schedule, "to_pandas"): schedule = schedule.to_pandas()
-        today = datetime.now().strftime('%Y-%m-%d')
-        upcoming = schedule[schedule['gameday'] >= today]
-        return int(upcoming['week'].min()) if not upcoming.empty else 18
+        #schedule = load_data_with_retry(lambda: nfl.load_schedules(seasons=[CURRENT_SEASON]), "Schedule Check")
+        #if hasattr(schedule, "to_pandas"): schedule = schedule.to_pandas()
+        #today = datetime.now().strftime('%Y-%m-%d')
+        #upcoming = schedule[schedule['gameday'] >= today]
+        #return int(upcoming['week'].min()) if not upcoming.empty else 18
     except:
-        return 1
+        return 12
 
 def get_weather_forecast(home_team, game_dt_str, is_dome=False):
     # 1. Check if Game is Finished (Current Time > Game Time + 4 hours)
